93d020fcc32f47e2816880693f32b2b20ed1ecf3
yurii macbook pro <yurikorock777@gmail.com>
2025-10-03 21:29:29 +0300

    feat: add Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,10 @@
-export default function Header (){
-    return <><h>Header</h></>
+import Logo from "../Logo/Logo.jsx";
+import  css from "./Header.module.css"
+
+export default function Header() {
+  return (
+    <header className={css.container}>
+      <Logo/>
+    </header>
+  );
 }
