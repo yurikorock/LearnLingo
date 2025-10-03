aa3963d6675f9f9d18b5345404ab1c0f46a4af5c
yurii macbook pro <yurikorock777@gmail.com>
2025-10-03 21:29:29 +0300

    feat: add Logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
-
 import "./App.css";
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout/Layout.jsx";
 
-
-
-
 export default function App() {
   return (
     <>
-      <h1>Best selling</h1>
-     <Layout></Layout>
+      <Layout>
+        <Routes>
+          <Route/>
+        </Routes>
+      </Layout>
     </>
   );
 }
