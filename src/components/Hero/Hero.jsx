cd8424f6b17c1b9acfd6d591653a41e939df04bb
yurii macbook pro <yurikorock777@gmail.com>
2025-10-04 18:27:39 +0300

    fix: NavLink to=/teachers

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,11 +8,13 @@ export default function Hero() {
           Unlock your potential with the best <span>language</span> tutors
         </h1>
         <p className={css.text}>
-          Embark on an Exciting Language Journey with Expert Language 
-          Tutors: Elevate your language proficiency to new heights by connecting with
+          Embark on an Exciting Language Journey with Expert Language Tutors:
+          Elevate your language proficiency to new heights by connecting with
           highly qualified and experienced tutors.
         </p>
-        <NavLink className={css.typeBTN}><span>Get started</span></NavLink>
+        <NavLink to="/teachers" className={css.typeBTN}>
+          <span>Get started</span>
+        </NavLink>
       </div>
       <div className={css.right_block}>RightBlock</div>
     </div>
