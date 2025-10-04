a51fedcd40fffe5152e200b77230893125b99887
yurii macbook pro <yurikorock777@gmail.com>
2025-10-04 10:00:02 +0300

    feat: add Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import Logo from "../Logo/Logo.jsx";
+import Navigation from "../Navigation/Navigation.jsx";
 import  css from "./Header.module.css"
 
 export default function Header() {
   return (
     <header className={css.container}>
       <Logo/>
+      <Navigation/>
     </header>
   );
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header.jsx";
-import css from "./Layout.module.css"
+import css from "./Layout.module.css";
 
-export default function Layout (){
-    return (<>
-        <Header/>
-        {/* <ModalContainer/> */}
-        <main className={css.main}>
-            <Outlet />
-          </main>
-        {/* <Footer/> */}
-    </>)
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      {/* <ModalContainer/> */}
+      <main className={css.main}>
+        <Outlet />
+      </main>
+      {/* <Footer/> */}
+    </>
+  );
 }
