ffff443ce713194a54473dbc1762fd5f5480eb38
yurii macbook pro <yurikorock777@gmail.com>
2025-10-04 18:25:37 +0300

    feat: add leftblock to MainPage

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,19 @@
+import { NavLink } from "react-router-dom";
 import css from "./Hero.module.css";
 export default function Hero() {
   return (
     <div className={css.container}>
-      <div className={css.left_block}>LeftBlock</div>
+      <div className={css.left_block}>
+        <h1 className={css.title}>
+          Unlock your potential with the best <span>language</span> tutors
+        </h1>
+        <p className={css.text}>
+          Embark on an Exciting Language Journey with Expert Language 
+          Tutors: Elevate your language proficiency to new heights by connecting with
+          highly qualified and experienced tutors.
+        </p>
+        <NavLink className={css.typeBTN}><span>Get started</span></NavLink>
+      </div>
       <div className={css.right_block}>RightBlock</div>
     </div>
   );
