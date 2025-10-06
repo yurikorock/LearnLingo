5536e090aecf6b23e2b602790695e10cbecb82dc
yurii macbook pro <yurikorock777@gmail.com>
2025-10-06 18:58:25 +0300

    feat: add react-select(language) to Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,27 +1,59 @@
 import { Field, Form, Formik } from "formik";
 import css from "./Filter.module.css";
 import { useId } from "react";
+import Select from "react-select";
 
 export default function Filter() {
   const languagesFieldId = useId();
 
+  const options = [
+    { value: "french", label: "French" },
+    { value: "english", label: "English" },
+    { value: "german", label: "German" },
+    { value: "ukrainian", label: "Ukrainian" },
+    { value: "polish", label: "Polish" },
+  ];
+
   const initialValues = {
-    languages: "french",
+    languages: "",
   };
 
   const handleSubmit = (values, actions) => {
-    console.log(values);
+    console.log(values.languages.value);
     actions.resetForm();
   };
   return (
     <div>
       <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-        <Form>
-          <div className={css.filter_languages}>
-            <label htmlFor={languagesFieldId} className={css.label}>
-              Languages
-            </label>
-            <Field
+        {({ setFieldValue, values }) => (
+          <Form>
+            <div className={css.filter_languages}>
+              <label htmlFor={languagesFieldId} className={css.label}>
+                Languages
+              </label>
+              <Select
+                inputId={languagesFieldId}
+                name="languages"
+                options={options}
+                value={values.languages}
+                onChange={(selectedOption) =>
+                  setFieldValue("languages", selectedOption)
+                }
+                placeholder="Choose a language"
+                classNamePrefix="react-select"
+              />
+            </div>
+
+            <button type="submit">Search</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+{
+  /* <Field
               as="select"
               name="languages"
               id={languagesFieldId}
@@ -32,12 +64,5 @@ export default function Filter() {
               <option value="german" className={css.option}>German</option>
               <option value="ukrainian" className={css.option}>Ukrainian</option>
               <option value="polish" className={css.option}>Polish</option>
-            </Field>
-          </div>
-
-          <button type="submit">Search</button>
-        </Form>
-      </Formik>
-    </div>
-  );
+            </Field> */
 }
