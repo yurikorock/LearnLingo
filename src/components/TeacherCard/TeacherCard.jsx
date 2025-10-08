872c5f279a4d3a73e1454ca4e53a681ad6acf2f9
yurii macbook pro <yurikorock777@gmail.com>
2025-10-08 22:04:49 +0300

    style: add to avatar in TeacherCard

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -1,9 +1,37 @@
-import css from "./TeacherCard.module.css"
+import css from "./TeacherCard.module.css";
 
-export default function TeacherCard({teacher}){
-    return (
-        <div>
-            <ul>{teacher.name}</ul>
+export default function TeacherCard({ teacher }) {
+  const {
+    id,
+    name,
+    surname,
+    languages,
+    levels,
+    rating,
+    reviews,
+    price_per_hour,
+    lessons_done,
+    avatar_url,
+    lesson_info,
+    conditions,
+    experience,
+  } = teacher;
+
+  return (
+    <div className={css.container}>
+      <div className={css.user}>
+        <div className={css.avatarWrap}>
+          <img
+            className={css.avatar}
+            src={avatar_url}
+            alt={`${name}${surname}`}
+          />
+          <svg className={css.is_online} width="12" height="12">
+            <use href="/sprite/sprite.svg#icon-green-circle"></use>
+          </svg>
         </div>
-    )
+      </div>
+      <ul>{teacher.name}</ul>
+    </div>
+  );
 }
