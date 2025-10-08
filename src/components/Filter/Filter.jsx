6870416dfae3ac1e4b9b5b1dd403c8fe0cbd104d
yurii macbook pro <yurikorock777@gmail.com>
2025-10-08 16:10:59 +0300

    feat: add levels-filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,21 +5,30 @@ import Select from "react-select";
 
 export default function Filter() {
   const languagesFieldId = useId();
+  const levelFieldId = useId();
 
-  const options = [
-    { value: "french", label: "French" },
+  const options = {
+    languages:[{ value: "french", label: "French" },
     { value: "english", label: "English" },
     { value: "german", label: "German" },
     { value: "ukrainian", label: "Ukrainian" },
-    { value: "polish", label: "Polish" },
-  ];
+    { value: "polish", label: "Polish" },],
+    levels: [{ value: "beginner", label: "A1 Beginner" },
+    { value: "elementary", label: "A2 Elementary" },
+    { value: "intermediate", label: "B1 Intermediate" },
+    { value: "upper-Intermediate", label: "B2 Upper-Intermediate" },],
+    
+  };
 
+  
   const initialValues = {
     languages: "",
+    levels: "",
   };
 
   const handleSubmit = (values, actions) => {
     console.log(values.languages.value);
+    console.log(values.levels.value);
     actions.resetForm();
   };
   return (
@@ -36,12 +45,31 @@ export default function Filter() {
                 isSearchable={false} // вимикаємо інпут повністю
                 inputId={languagesFieldId}
                 name="languages"
-                options={options}
+                options={options.languages}
                 value={values.languages}
                 onChange={(selectedOption) =>
                   setFieldValue("languages", selectedOption)
                 }
                 placeholder="Choose a language"
+                classNamePrefix="custom-select"
+              />
+            </div>
+
+            <div className={css.filter_languages}>
+              <label htmlFor={levelFieldId} className={css.label}>
+                Level of knowledge
+              </label>
+              <Select
+                unstyled //обнулили стилі
+                isSearchable={false} // вимикаємо інпут повністю
+                inputId={levelFieldId}
+                name="levels"
+                options={options.levels}
+                value={values.levels}
+                onChange={(selectedOption) =>
+                  setFieldValue("levels", selectedOption)
+                }
+                placeholder="Choose a level"
                 classNamePrefix="custom-select"
               />
             </div>
