638be6fa1a362a2b2a4b6afc43e2e977a2134d84
yurii macbook pro <yurikorock777@gmail.com>
2025-10-09 23:41:39 +0300

    feat: add teacher-info

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -19,7 +19,7 @@ export default function TeacherCard({ teacher }) {
 
   return (
     <div className={css.container}>
-      <div className={css.user}>
+      <section className={css.user}>
         <div className={css.avatarWrap}>
           <img
             className={css.avatar}
@@ -30,8 +30,35 @@ export default function TeacherCard({ teacher }) {
             <use href="/sprite/sprite.svg#icon-green-circle"></use>
           </svg>
         </div>
-      </div>
-      <ul>{teacher.name}</ul>
+      </section>
+      <section className={css.profile_info}>
+        <div className={css.teacher_info}>
+          <p className={css.languages}>Languages</p>
+          <h3 className={css.teacher_name}>{`${name} ${surname}`}</h3>
+          <ul className={css.rating}>
+            <li>
+              <svg className={css.icon_book} width="16" height="16">
+                <use href="/sprite/sprite.svg#icon-book-open"></use>
+              </svg>
+              <p>Lessons online</p>
+            </li>
+            <li>
+              <p>{`Lessons done: ${lessons_done}`}</p>
+            </li>
+            <li>
+              <svg className={css.icon} width="16" height="16">
+                <use href="/sprite/sprite.svg#icon-star"></use>
+              </svg>
+              <p>{`Rating: ${rating}`}</p>
+            </li>
+            <li>
+              <p>
+                Price / 1 hour: <span>{rating}$</span>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
