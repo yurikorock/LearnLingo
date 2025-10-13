a666a8a84a0275be84c54059f0419262b503f31f
yurii macbook pro <yurikorock777@gmail.com>
2025-10-13 19:11:10 +0300

    feat: work open close ModalBookTrial

--- src/components/ModalBookTrial/ModalBookTrial.jsx
+++ src/components/ModalBookTrial/ModalBookTrial.jsx
@@ -1,9 +1,41 @@
+import { useDispatch } from "react-redux";
 import css from "./ModalBookTrial.module.css";
+import { closeModal } from "../../redux/modal/slice.js";
+import { useEffect } from "react";
 
 export default function ModalBookTrial() {
+  const dispatch = useDispatch();
+  const handleClose = () => {
+    dispatch(closeModal());
+  };
+
+    // --- Закриття при кліку на backdrop ---
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
+  // --- Закриття при натисканні ESC ---
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        handleClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
-    <div className={css.backdrop}>
+    <div className={css.backdrop} onClick={handleBackdropClick}>
       <div className={css.modal}>
+        <button className={css.closeBtn} onClick={handleClose}>
+          <svg className={css.closeBtn_icon} width="32" height="32">
+            <use href="/sprite/sprite.svg#icon-close-x"></use>
+          </svg>
+        </button>
         <div className={css.title_wrap}>
           <h2 className={css.title}>Trial Lesson Modal</h2>
           <p className={css.descr}>
@@ -21,7 +53,9 @@ export default function ModalBookTrial() {
             <p className={css.select_name}>Jane Smith</p>
           </div>
         </div>
-        <h3 className={css.title_quest}>What is your main reason for learning English?</h3>
+        <h3 className={css.title_quest}>
+          What is your main reason for learning English?
+        </h3>
         <button className={css.modal_btn}>Book</button>
       </div>
     </div>
