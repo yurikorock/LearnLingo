c2d2747d0d9f0ddcd208c0b10fb87dbd88a1338a
yurii macbook pro <yurikorock777@gmail.com>
2025-10-18 21:09:48 +0300

    feat: add redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from 'redux-persist/integration/react'
 import "./firebase.js"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import modalReducer from "./modal/slice.js"
+import modalReducer from "./modal/slice.js";
+import userReducer from "./auth/userSlice.js";
 // import bookTrialReducer from "./modal/bookTrialSlice.js"
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "user-data",
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
-    reducer: {
-        modal: modalReducer,
-        // bookTrial: bookTrialReducer,
-    }
+  reducer: {
+    modal: modalReducer,
+    user: persistedUserReducer,
+    // bookTrial: bookTrialReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
