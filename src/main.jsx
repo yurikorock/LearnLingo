c8fe3718410b2d90a1396097b8134592c156614b
yurii macbook pro <yurikorock777@gmail.com>
2025-10-18 18:54:09 +0300

    feat: Initialize Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import "./firebase.js"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
