a90cb92cf0702abf8b74acb83a7ef0af1eaaa370
yurii macbook pro <yurikorock777@gmail.com>
2025-10-21 19:01:00 +0300

    feat: get teacherslist with metod firebase

--- src/redux/teachers/operations.js
+++ src/redux/teachers/operations.js
@@ -1,19 +1,45 @@
 //operations
 
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
 
-axios.defaults.baseURL = "https://learnlingo-v-1-1-default-rtdb.firebaseio.com";
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { get, ref, child } from "firebase/database";
+import {db} from "../../firebase.js"
 
 export const getTeachersList = createAsyncThunk(
-  "get/getTeachersList",
+  "teachers/getTeachersList",
   async (_, thunkAPI) => {
     try {
-      const {data} = await axios.get("/.json");
-      const teachersArray = Object.values(data || {});
-      return teachersArray;
+      const dbRef = ref(db);
+      const snapshot = await get(child(dbRef, "/"));
+      if (snapshot.exists()) {
+        const teachersArray = Object.values(snapshot.val());
+        return teachersArray;
+      } else {
+        return [];
+      }
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+
+// GET WITH AXIOS //
+
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// axios.defaults.baseURL = "https://learnlingo-v-1-1-default-rtdb.firebaseio.com";
+
+// export const getTeachersList = createAsyncThunk(
+//   "get/getTeachersList",
+//   async (_, thunkAPI) => {
+//     try {
+//       const {data} = await axios.get("/.json");
+//       const teachersArray = Object.values(data || {});
+//       return teachersArray;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
