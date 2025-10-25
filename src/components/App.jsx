a5f40dac9560d03f41f78e5ae36246b8591c7a67
yurii macbook pro <yurikorock777@gmail.com>
2025-10-25 17:19:40 +0300

    feat: add lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,33 @@
 import "./App.css";
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout/Layout.jsx";
-import MainPage from "../pages/MainPage/MainPage.jsx";
-import TeachersPage from "../pages/TeachersPage/TeachersPage.jsx";
-import UserDetailsPage from "./UserDetails/UserDetails.jsx";
-import AuthPage from "../pages/AuthPage.jsx/AuthPage.jsx";
 import { Toaster } from "react-hot-toast";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.jsx";
-import FavoritesPage from "../pages/FavoritesPage/FavoritesPage.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
+
+const Layout = lazy(()=> import("./Layout/Layout.jsx"));
+const MainPage = lazy(()=> import("../pages/MainPage/MainPage.jsx"));
+const TeachersPage = lazy(()=> import("../pages/TeachersPage/TeachersPage.jsx"));
+const FavoritesPage  = lazy(()=> import("../pages/FavoritesPage/FavoritesPage.jsx"));
+
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="/teachers" element={<TeachersPage />} />
-        </Route>
-        <Route
-          path="/favorites"
-          element={
-            <PrivateRoute component={FavoritesPage} redirectTo="/" />
-          }
-        />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="/teachers" element={<TeachersPage />} />
+          </Route>
+          <Route
+            path="/favorites"
+            element={<PrivateRoute component={FavoritesPage} redirectTo="/" />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+
       <Toaster position="top-right" reverseOrder={false} />
     </>
   );
